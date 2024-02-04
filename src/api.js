c7e789a89a130d61333b7d3556a3fb8b4a48bb6e
ATN04 <95452323+EtienneK2004@users.noreply.github.com>
2024-02-04 21:59:44 +0100

    étape 3

--- src/api.js
+++ src/api.js
@@ -1,4 +1,8 @@
+import crypto from 'crypto';
+import fetch from 'node-fetch';
 
+const PUBLIC_KEY="4522b0d1398c1f774f0fccbb056bfc4e"
+const PRIVATE_KEY="877c848450ca4fd299efda6388c6b15eca8cb454"
 
 /**
  * Récupère les données de l'endpoint en utilisant les identifiants
@@ -7,7 +11,23 @@
  * @return {Promise<json>}
  */
 export const getData = async (url) => {
-    // A Compléter
+    const ts = new Date().getTime().toString();
+    const hash = await getHash(PUBLIC_KEY, PRIVATE_KEY, ts);
+
+    const noParams = url.indexOf('?') === -1;
+    const debutParam = noParams ? '?' : '&';
+
+    url = url + debutParam + 'ts=' + ts + '&apikey=' + PUBLIC_KEY + '&hash=' + hash;
+
+    const res = await fetch(url).then((res) => res.json());
+    const triThumbnailValid = res.data.results.filter((char) => {
+        return char.thumbnail.path.indexOf('image_not_available') === -1;
+    });
+    const ajoutImageUrl = triThumbnailValid.map((char) => {
+        char.imageUrl = char.thumbnail.path + '.' + char.thumbnail.extension;
+        return char;
+    });
+    return ajoutImageUrl;
 }
 
 /**
@@ -19,5 +39,5 @@ export const getData = async (url) => {
  * @return {Promise<ArrayBuffer>} en hexadecimal
  */
 export const getHash = async (publicKey, privateKey, timestamp) => {
-    // A compléter
+    return crypto.createHash('md5').update(""+ timestamp + privateKey + publicKey).digest('hex').toString();
 }
