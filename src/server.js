4bc3b55ef2daacf3383defc5df89fcba8bf4d41a
ATN04 <95452323+EtienneK2004@users.noreply.github.com>
2024-02-04 21:59:58 +0100

    étape 4

--- src/server.js
+++ src/server.js
@@ -1,1 +1,42 @@
-console.log("Je suis Marvel")
+import Fastify from 'fastify';
+
+import {getData } from "./api.js";
+import fastifyStatic from "@fastify/static";
+import path from "path";
+import handlebars from 'handlebars';
+import fastifyView from '@fastify/view';
+
+
+
+const app = Fastify();
+const __dirname = path.resolve();
+
+app.register(fastifyStatic, {
+    root: path.join(__dirname, 'templates')
+});
+app.register(fastifyView, {
+    engine: {
+        handlebars: handlebars
+    },
+    templates: 'templates',
+    options: {
+        partials: {
+            header: path.join('header.hbs'),
+            footer: path.join('footer.hbs')
+        }
+    }
+});
+
+app.get('/', (request, response) => {
+
+    response.headers({'Content-Type': 'text/html'});
+    getData("https://gateway.marvel.com:443/v1/public/characters").then(data => {
+        response.view("index.hbs", { data: data });
+    }).catch(err => {
+        console.error("Erreur : ", err);
+        response.status(500).send(err.message);
+    });
+
+});
+
+app.listen({ port: 3000, host: 'localhost' });
